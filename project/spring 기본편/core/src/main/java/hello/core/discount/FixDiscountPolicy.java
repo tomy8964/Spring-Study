793998b4aab4630441cb11354766ae5d76dffdf9
tomy8964 <tomy8964@naver.com>
2022-12-29 00:09:32 +0900

    12.29

---
+++ project/spring 기본편/core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,16 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+    private int discountFixAmount = 1000; //1000원 고정 할인
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}
