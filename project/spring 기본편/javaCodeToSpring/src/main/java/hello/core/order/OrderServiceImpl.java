cc2328bfc622c06ec7a74c032339ea7d840aa3a5
tomy8964 <tomy8964@naver.com>
2023-01-03 00:08:30 +0900

    01.03

--- project/spring 기본편/javaCodeToSpring/src/main/java/hello/core/order/OrderServiceImpl.java
+++ project/spring 기본편/javaCodeToSpring/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,9 +1,12 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,7 +15,7 @@ public class OrderServiceImpl implements OrderService{
     private final DiscountPolicy discountPolicy;
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
