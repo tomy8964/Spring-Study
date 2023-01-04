205db58d93fa1de7eaa804a8e19dd1fb6421ed97
Ham Geonwook <103511161+tomy8964@users.noreply.github.com>
2023-01-05 00:11:43 +0900

    Create LogDemoService.java

---
+++ project/spring 기본편/javaCodeToSpring/src/main/java/hello/core/web/LogDemoService.java
@@ -0,0 +1,9 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
