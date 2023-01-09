92f91dfbd69720f1d4ed4e1fe57f132d7e800afa
tomy8964 <tomy8964@naver.com>
2023-01-10 00:00:19 +0900

    01.10

---
+++ project/스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술/exercise/item-service/src/test/java/hellod/itemservice/ItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hellod.itemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
