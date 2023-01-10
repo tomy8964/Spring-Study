d970f1a01943f2e10c8f3313ca021be44ab86120
tomy8964 <tomy8964@naver.com>
2023-01-10 17:32:20 +0900

    01.10 spring MVC - make webpage, done 1st MVC lecture

--- project/스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술/exercise/item-service/src/main/java/hellod/itemservice/web/basic/BasicItemController.java
+++ project/스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술/exercise/item-service/src/main/java/hellod/itemservice/web/basic/BasicItemController.java
@@ -7,8 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -24,6 +24,98 @@ public class BasicItemController {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findByID(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item) {
+        itemRepository.save(item);
+
+//        model.addAttribute("item", item);
+//        자동 추가, 생략 가능
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+//        model.addAttribute("item", item);
+//        자동 추가, 생략 가능
+//        class명인 Item -> item으로 바뀌어서 자동 등록
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV4(Item item) {
+//        model.addAttribute("item", item);
+//        자동 추가, 생략 가능
+//        class명인 Item -> item으로 바뀌어서 자동 등록
+//        @ModelAttribute 생략 가능
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV5(Item item) {
+//        model.addAttribute("item", item);
+//        자동 추가, 생략 가능
+//        class명인 Item -> item으로 바뀌어서 자동 등록
+//        @ModelAttribute 생략 가능
+        itemRepository.save(item);
+        //Post -> redirect -> Get
+        return "redirect:/basic/items/" + item.getId();
+    }
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+//        model.addAttribute("item", item);
+//        자동 추가, 생략 가능
+//        class명인 Item -> item으로 바뀌어서 자동 등록
+//        @ModelAttribute 생략 가능
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        //Post -> redirect -> Get
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editGet(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findByID(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String editPost(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
     }
 
     /**
