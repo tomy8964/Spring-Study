c4aa46b6c393dbe535ba5de6f62691181c5f2660
tomy8964 <tomy8964@naver.com>
2023-01-15 16:13:08 +0900

    01.15 - 로그인 처리2(필터, 인터셉터)

--- project/스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/exercise-mvc2/login/src/main/java/hello/login/web/login/LoginController.java
+++ project/스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/exercise-mvc2/login/src/main/java/hello/login/web/login/LoginController.java
@@ -12,6 +12,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -79,7 +80,7 @@ public class LoginController {
 
     }
 
-    @PostMapping("/login")
+//    @PostMapping("/login")
     public String loginV3(@Validated @ModelAttribute("loginForm") LoginForm form,
                           BindingResult bindingResult,
                           HttpServletRequest request) {
@@ -104,6 +105,35 @@ public class LoginController {
 //        sessionManager.createSession(loginMember, response);
 
         return "redirect:/";
+
+    }
+
+    @PostMapping("/login")
+    public String loginV4(@Validated @ModelAttribute("loginForm") LoginForm form,
+                          BindingResult bindingResult,
+                          @RequestParam(defaultValue = "/") String redirectURL,
+                          HttpServletRequest request) {
+
+        if (bindingResult.hasErrors()) {
+            return "/login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "/login/loginForm";
+        }
+
+        //로그인 성공 처리
+        //세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성
+        HttpSession session = request.getSession();
+        //세션에 로긍니 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+        //세션 관리자를 통해 세션을 생성하고, 회원 데이터 보관
+//        sessionManager.createSession(loginMember, response);
+
+        return "redirect:" + redirectURL;
 
     }
 
