696b169076a2e9986959d39a0aa9eef747214f3d
tomy8964 <tomy8964@naver.com>
2023-01-20 00:07:23 +0900

    01.20 - JDBC 이해, 커넥션풀과 데이터소스 이해

---
+++ project/스프링 DB 1편 - 데이터 접근 핵심 원리/jdbcEx/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -0,0 +1,41 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+class MemberRepositoryV0Test {
+
+    MemberRepositoryV0 repository = new MemberRepositoryV0();
+
+    @Test
+    void crud() throws SQLException {
+        //save
+        Member member = new Member("memberV100", 10000);
+        repository.save(member);
+
+        //findById
+        Member findMember = repository.finById(member.getMemberId());
+        log.info("findMember = {}", findMember);
+        assertThat(findMember).isEqualTo(member);
+
+        //update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.finById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.finById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+
+    }
+}
