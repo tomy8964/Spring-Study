fc26be420748b344beb773e22062e31a33f2827e
tomy8964 <tomy8964@naver.com>
2023-01-23 03:13:59 +0900

    01.23 - 데이터 접근 기술(시작, 스프링 JdbcTemplate, 테스트, MyBatis)

--- project/스프링 DB 2편 - 데이터 접근 활용 기술/ex/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ project/스프링 DB 2편 - 데이터 접근 활용 기술/ex/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,7 +1,8 @@
 package hello.itemservice;
 
-import hello.itemservice.config.*;
+import hello.itemservice.config.MyBatisConfig;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +11,11 @@ import org.springframework.context.annotation.Profile;
 
 
 //@Import(MemoryConfig.class)
-@Import(JdbcTemplateV1Config.class)
+//@Import(JdbcTemplateV1Config.class)
+//@Import(JdbcTemplateV2Config.class)
+//@Import(JdbcTemplateV3Config.class)
+@Import(MyBatisConfig.class)
+@Slf4j
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
@@ -24,4 +29,17 @@ public class ItemServiceApplication {
 		return new TestDataInit(itemRepository);
 	}
 
+	/*
+	@Bean
+	@Profile("test")
+	public DataSource dataSource() {
+		log.info("메모리 데이터베이스 초기화");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
+	}
+	 */
 }
