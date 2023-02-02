9a7c09521300f94855a2ea4ed9beeef7e22fb66d
tomy8964 <tomy8964@naver.com>
2023-02-02 18:59:06 +0900

    domain

---
+++ spring boot & JPA/jpa 활용 1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+public abstract class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+}
